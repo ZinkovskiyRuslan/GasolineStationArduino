ebcfe66a1d9a396e422b293c1bc1ec3e7f6a7112
ZinkovskiyRV <ruslan.zinkovskiy@open.ru>
2021-11-25 19:29:12 +0400

    get fuel info from Wi-Fi

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -1,10 +1,20 @@
-
 #include "BluetoothSerial.h"
 #include "esp_bt_main.h"
 #include "esp_bt_device.h"
+//#include <Arduino.h>
+//#include <WiFi.h> 
+#include <WiFiMulti.h>
+//#include "Esp.h"
 
-BluetoothSerial ESP_BT; // Объект для Bluetooth
- 
+const char* ssid     = "3.14";
+const char* password = "0123456789";
+int retryFindWiFi = 5;
+
+const uint16_t port = 80;
+const char * host = "erpelement.ru"; // ip or dns
+
+BluetoothSerial ESP_BT; // Объект для Bluetooth 
+
 String incoming;
 int LED_BUILTIN = 12;
 
@@ -24,34 +34,14 @@ struct ClientFuelInfo
 struct Command Command;
 struct ClientFuelInfo ClientFuelInfo[1000];
 
-void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
-  if(event == ESP_SPP_SRV_OPEN_EVT){
-    Serial.println("Client Connected");
-  }
-
-  if(event == ESP_SPP_CLOSE_EVT ){
-    Serial.println("Client disconnected");
-  }
-}
-
 void setup() {
   Serial.begin(115200); // Запускаем последовательный монитор 
+  Serial.println("\n");
+  getFuelInfo();
   ESP_BT.begin("АЗС"); // Задаем имя вашего устройства Bluetooth
-  ESP_BT.register_callback(callback);
-  
+  ESP_BT.register_callback(callback);  
   pinMode (LED_BUILTIN, OUTPUT);// задаем контакт подключения светодиода как выходной
   printDeviceAddress();
-
-  //fill test data
-  for (int i=0; i<999; i++)
-  {
-      ClientFuelInfo[i].id  = "->" + String(i) + "<-";
-      ClientFuelInfo[i].fuel = i;
-      if (i==900)
-      {
-        ClientFuelInfo[i].id = "b73147eb6ff3e51b";
-      }
-  }
 }
  
 void loop() {
@@ -91,7 +81,7 @@ void loop() {
       }
       if(command == Command.StartFuelFill)
       {
-        for (int i=0; i<999; i++)
+        for (int i=0; i< (sizeof ClientFuelInfo/sizeof ClientFuelInfo[0]); i++)
         {
           if(ClientFuelInfo[i].id == deviceUniqueID)
           {
@@ -105,7 +95,7 @@ void loop() {
                   Serial.println(">>> " + ClientFuelInfo[i].id +"|"+Command.StartFuelFill+"|"+ j);
                   ESP_BT.println(ClientFuelInfo[i].id +"|"+Command.StartFuelFill+"|"+ j);
                 }
-                ClientFuelInfo[i].fuel=100;
+                ClientFuelInfo[i].fuel=0;
             }
           }
         }
@@ -113,12 +103,27 @@ void loop() {
     }    
   }  
   delay(100);
+  //Serial.println(ESP.getFreeHeap());
 }
 
-  
-  
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+  if(event == ESP_SPP_SRV_OPEN_EVT){
+    Serial.println("Client Connected");
+  }
 
+  if(event == ESP_SPP_CLOSE_EVT ){
+    Serial.println("Client disconnected");
+    //ESP.restart();
+    delay(1000);
+    ESP_BT.end();
+    delay(1000);
+    ESP_BT.begin("АЗС");
+    delay(1000);
+  }
+}
+   
 void printDeviceAddress() { 
+  Serial.print("Bluetooth: ");
   const uint8_t* point = esp_bt_dev_get_address(); 
   for (int i = 0; i < 6; i++) { 
     char str[3]; 
@@ -129,4 +134,83 @@ void printDeviceAddress() {
     } 
   }  
   Serial.println(""); 
+}
+
+void getFuelInfo()
+{
+  String line="";
+  WiFiMulti WiFiMulti;
+  WiFiMulti.addAP(ssid, password);  
+  Serial.print("Waiting for WiFi");
+  int retry = 0;
+  while(WiFiMulti.run() != WL_CONNECTED) {
+      Serial.print(".");
+      //delay(100);
+      if (retry > retryFindWiFi)
+        {
+          Serial.println("\nError Find WiFi");
+          return;
+        }
+      retry++;
+  }
+  Serial.println("\nWiFi connected. IP address: " + WiFi.localIP().toString());
+  Serial.println("Connecting to " + String(host));
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+      Serial.println("Connection failed.");
+      Serial.println("Waiting 5 seconds before retrying...");
+      delay(5000);
+      return;
+  }
+  client.println("GET /getFuelInfo.php HTTP/1.1");
+  client.println("Host: " + String(host) + " \n\n");
+
+  int maxloops = 0;
+  while (!client.available() && maxloops < 1000)
+  {
+    maxloops++;
+    delay(10); //delay 1 msec
+  }
+  if (client.available() > 0)
+  {
+    while(client.available() > 0)
+    {        
+      line = client.readStringUntil('\n');
+    }
+    Serial.println(line);
+  }
+  else
+  {
+    Serial.println("client.available() timed out ");
+  }
+  Serial.println("Closing connection.");
+  client.stop();  
+  WiFi.mode(WIFI_OFF);
+
+  //set empty
+  for (int i=0; i< (sizeof ClientFuelInfo/sizeof ClientFuelInfo[0]); i++)
+  {
+      ClientFuelInfo[i].id  = "";
+      ClientFuelInfo[i].fuel = 0;
+  }
+  
+  int i = 0;
+  String subStr = "";     
+  for (auto c : line)
+  {
+      if(c==';')
+      {
+          String id="";
+          String fuel="";
+
+          int index = subStr.indexOf('|');
+          ClientFuelInfo[i].id = subStr.substring(0, index);
+          ClientFuelInfo[i].fuel = subStr.substring(index + 1,subStr.length()).toInt();
+          printf("curr=%i id=%s fuel=%i\n", i, ClientFuelInfo[i].id.c_str(), ClientFuelInfo[i].fuel);
+          i++;
+          subStr = "";
+      }else{
+          subStr += c;
+      }        
+  }
 }
